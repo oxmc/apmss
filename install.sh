d0a886ac7c6d72e2d2806fffe2b93225105448f8
oxmc <67136658+oxmc@users.noreply.github.com>
2020-11-13 00:22:41 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,28 +5,6 @@ todaydate="$(date +"%A,%B,%d,%Y")"
 LATEST_VER="$(wget -qO- https://raw.githubusercontent.com/oxmc/apmss/main/data/version.txt)"
 LOCAL_VER="$(cat "${workdir}/data/version.txt")"
 #Functions
-progressfilt ()
-{
-    local flag=false c count cr=$'\r' nl=$'\n'
-    while IFS='' read -d '' -rn 1 c
-    do
-        if $flag
-        then
-            printf '%s' "$c"
-        else
-            if [[ $c != $cr && $c != $nl ]]
-            then
-                count=0
-            else
-                ((count++))
-                if ((count > 1))
-                then
-                    flag=true
-                fi
-            fi
-        fi
-    done
-}
 checkinstalledpack()
 {
   echo "Checking if required packages are installed"
@@ -42,7 +20,7 @@ checkforupdate()
       	--text="Version ${LATEST_VER} is available. Install now?" \
       	--center --text-align=center \
       	--button="Not now!${workdir}/icons/forward.png:1" \
-      	--button="Install!${workdir}/icons/16/downloadicon.png:0" 2>/dev/null && wget -O - https://raw.githubusercontent.com/oxmc/apmss/master/update | bash
+      	--button="Install!${workdir}/icons/16/downloadicon.png:0" 2>/dev/null && wget -O - https://raw.githubusercontent.com/oxmc/apmss/master/install | bash
       else
           echo "No updates availible"
 fi
